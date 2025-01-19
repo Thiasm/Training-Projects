bf087ceeac4255fd82cb775d16aa342154619e5f
Thias <mathias.roulleau@gmail.com>
2025-01-19 21:18:43 +0100

    Move Listhias to dedicated repo

--- Listhias/app/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def split(value, delimiter):
-    if isinstance(value, str):
-        return value.split(delimiter)
-    return value
--- Listhias/listhias/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-handler404 = 'app.views.custom_page_not_found_view'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls'))
-]
